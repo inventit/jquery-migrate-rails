a3bbba723867db920a1d0b2ee38d1f5bf2ae83f2
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 10:49:57 +0900

    initial commit

---
+++ lib/jquery/migrate/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/migrate/rails/version"
+
+module Jquery
+  module Migrate
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/migrate/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Migrate
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
